13dfb62fd43318a7ad4b3e6446c8da6429a2fe9c
Thomas Hervey <h46@umbc.edu>
2014-02-10 11:33:21 -0500

    change config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ DemoApp::Application.routes.draw do
 
   resources :users
 
+  root :to => 'users#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
